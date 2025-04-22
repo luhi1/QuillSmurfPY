3c80541d34e105bc3f5dfe5f4212f11a29094480
luhi1 <lulhi1@proton.me>
2025-04-22 15:54:43 -0700

    Cleaned Up Code and Merged Dev Branch

--- main.py
+++ main.py
@@ -4,106 +4,105 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException, TimeoutException
-import multiprocessing
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException, TimeoutException, ElementClickInterceptedException
+from selenium.webdriver.common.action_chains import ActionChains
+import nltk
+import nltk.tokenize
+import os 
 
+#To Do:
+#Sort the final output to not just be on one line.
+
+'''
+Install firefox first, latest version preferred. 
+
+
+pip install nltk
+pip install selenium
+
+Options : linux-aarch64, linux32, linux64
+Answer Based on: https://askubuntu.com/questions/870530/how-to-install-geckodriver-in-ubuntu
+
+export GECKO_DRIVER_VERSION='v0.36.0'
+export OS='INSERT OS NAME HERE'
+wget https://github.com/mozilla/geckodriver/releases/download/$GECKO_DRIVER_VERSION/geckodriver-$GECKO_DRIVER_VERSION-$OS.tar.gz
+tar -xvzf geckodriver-$GECKO_DRIVER_VERSION-$OS.tar.gz
+rm geckodriver-$GECKO_DRIVER_VERSION-$OS.tar.gz
+chmod +x geckodriver
+cp geckodriver /usr/local/bin/
+
+Info on non-linux install this will vary: https://selenium-python.readthedocs.io/installation.html#drivers
+'''
 def main():
-    browsers = [setupBrowser(r'C:\Users\walrus\AppData\Roaming\Mozilla\Firefox\Profiles\9zb8tz51.default')]
-    newBrowser: multiprocessing.pool.AsyncResult
-    for i in range (0,10):
-        if i % 3 == 0:
-            pool = multiprocessing.Pool(processes=1) 
-            newBrowser = pool.apply_async(setupBrowser, args = (r'C:\Users\walrus\Desktop\QuillSmurfPY'))
-             
-        writeQB(browsers[0], "Hi darling, I think you're really pretty and cool." + str(i+1))
-        if (i+1) % 3 == 0:
-            WebDriverWait(browsers[0], 20).until(EC.visibility_of_element_located((By.XPATH, "//div[@id='root-client']/div/div[4]/div")))
-            browsers[0].quit()
-            del browsers[0]
-            browsers[newBrowser.get()]
-    WebDriverWait(browsers[0], 20).until(EC.visibility_of_element_located((By.XPATH, "//div[@id='root-client']/div/div[4]/div")))
-    browsers[0].quit()
+    nltk.download('punkt_tab')
+    print("Welcome to Quill Smurf!\n")
+    inputText = input("Enter text to paraphrase:")
+    outputDir = input("\nEnter the full path name (or leave blank for the same path as this executable) where the output should be stored. \n" +
+                      "For example: C:\\\\username\Downloads\outputDir\. \n"
+                      +"INCLUDE A TRAILING '/' or \\'.\n"
+                      "WARNING: IF THERE IS A FILE NAMED 'quillSmurfOutput.txt' IT WILL BE DELETED \n" +
+                      "Enter here: ")
+    outputDir = outputDir+"quillSmurfOutput.txt"
+    inputTextArray = nltk.tokenize.sent_tokenize(inputText)
+    sortedInputTextArray = []
+    pSentence = inputTextArray[0]
+    i = 1
+    while i < len(inputTextArray):
+        if len(pSentence.split(" ")) + len(inputTextArray[i].split(" ")) < 125:
+            pSentence = pSentence + " " + inputTextArray[i]
+            i += 1
+        else:
+            sortedInputTextArray.append(pSentence)
+            pSentence = ""
+    sortedInputTextArray.append(pSentence)
+    print("This bot just saved you " + str(len(sortedInputTextArray)) + " copy and paste sequence(s).")
 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-#multithreading makes me really happy 
-def setupBrowser(profile):
+    if os.path.exists(outputDir):
+        os.remove(outputDir)
+        print("Successfully removed your prior output.")
+
+
+    outputFile = open(outputDir, "a")
+    browser = setupBrowser()  
+    for i in range (0,len(sortedInputTextArray)):
+        writeQB(browser, sortedInputTextArray[i], outputFile)
+
+        if (i == len(sortedInputTextArray)-1):
+            browser.quit()
+        elif ((i+1) % 3 == 0):
+            browser.quit()
+            browser = setupBrowser()
+    sortOutput()
+    outputFile.close()
+    
+def sortOutput(outputFile):
+    #TODO: Implement!
+def setupBrowser():
     uOptions = Options()
     #uOptions.add_argument("--headless")
-    uOptions.add_argument("--window-size=1920,1200")
-    uOptions.add_argument("--profile")
-    uOptions.add_argument(profile)
-    uOptions.set_preference("browser.download.folderList", 2)
-    uOptions.set_preference("browser.download.dir", "C:\\Users\walrus\Desktop\QuillSmurfPY")
     browser = webdriver.Firefox(options=uOptions)
     browser.get('https://quillbot.com/paraphrasing-tool')
     loadQB(browser) 
     return browser
 
 def loadQB(browser):
-    loadedIndicator = browser.find_element(By.XPATH, "//div[@id='root-client']/div[1]")
-    while (loadedIndicator.get_attribute("style") != "--header-height: 53px; --banner-height: 68px;"):
-        WebDriverWait(browser, 0.5)
-
+    
     try:
+        WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH,'//iframe[@title="Sign in with Google Dialog"]')))
         browser.switch_to.frame(browser.find_element(By.XPATH,'//iframe[@title="Sign in with Google Dialog"]'))
         browser.find_element(By.ID, "close").click()
         browser.switch_to.parent_frame()
     except NoSuchElementException:
         pass
 
-def writeQB(browser, text):
+
+def writeQB(browser, text, outputFile):
     input = browser.find_element(By.ID, "paraphraser-input-box")
     input.send_keys(Keys.CONTROL + "a")
     input.send_keys(Keys.DELETE)
     input.send_keys(text)  
 
-    pButton = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button[data-testid='pphr/input_footer/paraphrase_button']")))
+    pButton = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-testid='pphr/input_footer/paraphrase_button']")))
     while (not(EC.staleness_of(pButton)(browser))):
         pButton.click()
 
@@ -117,7 +116,27 @@ def writeQB(browser, text):
 
     exportButton = WebDriverWait(browser,20).until(EC.visibility_of_element_located((By.XPATH, "//button[@data-testid='pphr/output_footer/export_button']")))
     WebDriverWait(browser, 20).until(EC.element_to_be_clickable(exportButton))
-    browser.execute_script("arguments[0].click();", exportButton)
 
+    # Circumventing the split up div by copying output somewhere else.
+    # Think of this next block of code as just getting the output.
+    try:
+        copyButton = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH, "//button[@data-testid='pphr/output_footer/copy_text_button']")))
+        copyButton.click()
+    except (ElementClickInterceptedException):
+        xButton = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-testid='styleq-dialog-close-button']")))
+        xButton.click()
+        copyButton = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH, "//button[@data-testid='pphr/output_footer/copy_text_button']")))
+        copyButton.click()
+
+    input = browser.find_element(By.ID, "paraphraser-input-box")
+    input.send_keys(Keys.CONTROL + "a")
+    input.send_keys(Keys.DELETE)
+    input.send_keys(Keys.CONTROL + "v")
+    finalText = input.text
+    
+    print(finalText)
+    outputFile.write(finalText)
+
+    
 if __name__ == "__main__":
     main()
